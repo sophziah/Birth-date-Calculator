da99228921107ff12f434ea9e25e0b9e706c4844
Sophia Keziah <141401003+sophziah@users.noreply.github.com>
2024-10-02 16:47:31 +0800

    Update birth-date-calculator.py

--- birth-date-calculator.py
+++ birth-date-calculator.py
@@ -1,6 +1,6 @@
 import datetime
 
-def calculate_age(birth_date):
+def calculate_age(birth_date_str):
     """Calculates the age based on the birth date and current date.
 
     Args:
@@ -9,13 +9,24 @@ def calculate_age(birth_date):
     Returns:
         int: The calculated age.
     """
+# make a function called calculate_age, with user input (birth_date_str)
+def calculate_age(birth_date_str):
 
-    birth_date = datetime.datetime.strptime(birth_date, "%m-%d-%Y")
-    today = datetime.datetime.now()
+# datetime.datetime fixed         
+    birth_date = datetime.datetime.strptime(birth_date_str, "%d-%m-%Y")
+    today = datetime.date.today()
+
+#                                             date today       BEFORE <         actual birthday 
     age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
+
+# calculation
     return age
 
-if __name__ == "__main__":
-    birth_date = input("Birth Date (mm-dd-yyyy): ")
-    age = calculate_age(birth_date)
-    print("Your age is", age)
+# Get user input for birth date
+birth_date_input = input("Birth Date (dd-mm-yyyy): ")
+
+# Calculate and print the age and call the function (calculate_age)
+age = calculate_age(birth_date_input)
+
+# displays calculation 
+print("Your age is:", age)
